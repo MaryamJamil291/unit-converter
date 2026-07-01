b69f6554dcf63e8362004402fce19ba3dcead3ac
Your Name <your_email@example.com>
2026-07-01 16:39:25 +0500

    Add Multi unit converter

---
+++ Multi unit converter.py
@@ -0,0 +1,27 @@
+Num1=input("enter any number:")
+
+number1=float(Num1)
+
+# 1= Kilometer to meter
+print("km to m=",number1*1000,"m")
+
+# 2= meter to kilokmeter
+print("m to Km=",number1/1000,"km")
+
+# 3= inches to centeimeter
+print("inch to cm=",number1*2.54,"cm")
+
+# 4= centimeter to inches
+print("cm to inch=",number1/2.54,"inch")
+
+# 5= feet to meters
+print("feet to m=",number1*0.3048,"m")
+
+# 6= meters to feet
+print("m to feet=",number1/0.3048,"feet")
+
+# 7= miles to kilometer
+print("miles to km=",number1*1.60934,"km")
+
+# 8= kilometer to miles
+print("km to miles=",number1/1.60934,"miles")
